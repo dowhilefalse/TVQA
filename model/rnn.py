51dd27faa927e1fe853eab0ac226012f4240c8ed
Zineng Tang <37575766+dowhilefalse@users.noreply.github.com>
2019-08-31 11:24:35 -0400

    Update rnn.py

--- model/rnn.py
+++ model/rnn.py
@@ -5,6 +5,24 @@ __author__ = "Jie Lei"
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+tf.reset_default_graph()
+
+# Create input data
+X = np.random.randn(2, 10, 8)
+
+# The second example is of length 6 
+X[1,6,:] = 0
+X_lengths = [10, 6]
+def RNNEncoder():
+    cell = tf.nn.rnn_cell.LSTMCell(num_units=64, state_is_tuple=True)
+    cell = tf.nn.rnn_cell.DropoutWrapper(cell=cell, output_keep_prob=0.5)
+    cell = tf.nn.rnn_cell.MultiRNNCell(cells=[cell] * 4, state_is_tuple=True)
+
+    outputs, last_states = tf.nn.dynamic_rnn(
+        cell=cell,
+        dtype=tf.float64,
+        sequence_length=X_lengths,
+        inputs=X)
 
 
 class RNNEncoder(nn.Module):
