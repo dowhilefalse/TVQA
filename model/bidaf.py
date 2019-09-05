5554c09539228eefcf988329c4650f1e83414aed
Zineng Tang <37575766+dowhilefalse@users.noreply.github.com>
2019-09-04 23:13:40 -0400

    Update bidaf.py

--- model/bidaf.py
+++ model/bidaf.py
@@ -1,9 +1,9 @@
-__author__ = "Jie Lei"
+# __author__ = "Jie Lei"
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.autograd import Variable
 import tensorflow as tf
 
 class BidafAttn(nn.Module):
@@ -46,8 +46,8 @@ class BidafAttn(nn.Module):
         :return:
         """
         if self.method == "original":
-            t1 = s1.size(1)
-            t2 = s2.size(1)
+            t1 = s1.shape[1]
+            t2 = s2.shape[1]
             repeat_s1 = tf.tile(tf.expand_dims(t1, 2), [1, 1, t2, 1])
             repeat_s2 = tf.tile(tf.expand_dims(t1, 2), [1, t1, 1, 1])
             packed_s1_s2 = tf.concat([repeat_s1, repeat_s2, repeat_s1 * repeat_s2], 3)
@@ -61,13 +61,14 @@ class BidafAttn(nn.Module):
             s = tf.matmul(s1, tf.transpose(s2, [1, 2]))
 #             s = torch.bmm(s1, s2.transpose(1, 2))
 
-        s_mask = s.data.new(*s.size()).fill_(1).byte()  # [B, T1, T2]
+        
+#         s_mask = s.data.new(*s.size()).fill_(1).byte()  # [B, T1, T2]
+        s_mask = tf.Variable(tf.ones_like(s))
         # Init similarity mask using lengths
         for i, (l_1, l_2) in enumerate(zip(l1, l2)):
-            s_mask[i][:l_1, :l_2] = 0
-
-        s_mask = Variable(s_mask)
-        s.data.masked_fill_(s_mask.data.byte(), -float("inf"))
+            tf.assign(s_mask[i][:l_1, :l_2], 0)
+        
+#         s.data.masked_fill_(s_mask.data.byte(), -float("inf"))
         return s
 
     @classmethod
@@ -77,12 +78,15 @@ class BidafAttn(nn.Module):
         signify which words in s2 are most relevant to words in s1
         """
         a_weight = tf.softmax(s, 2)  # [B, t1, t2]
-        a_weight.data.masked_fill_(a_weight.data != a_weight.data, 0)  # remove nan from softmax on -inf
-        u_tile = torch.bmm(a_weight, s2)  # [B, t1, t2] * [B, t2, D] -> [B, t1, D]
-
+        tf.assign(a_weight[tf.where(tf.is_nan(a_weight))], 0)
+#         a_weight.data.masked_fill_(a_weight.data != a_weight.data, 0)  # remove nan from softmax on -inf
+#         u_tile = torch.bmm(a_weight, s2)  # [B, t1, t2] * [B, t2, D] -> [B, t1, D]
+        u_til = tf.matmul(a_weight, s2)
         a_weight = F.softmax(s, dim=2)  # [B, t1, t2]
-        a_weight.data.masked_fill_(a_weight.data != a_weight.data, 0)  # remove nan from softmax on -inf
-        u_tile = torch.bmm(a_weight, s2)  # [B, t1, t2] * [B, t2, D] -> [B, t1, D]
+        tf.assign(a_weight[tf.where(tf.is_nan(a_weight))], 0)
+#         a_weight.data.masked_fill_(a_weight.data != a_weight.data, 0)  # remove nan from softmax on -inf
+#         u_tile = torch.bmm(a_weight, s2)  # [B, t1, t2] * [B, t2, D] -> [B, t1, D]
+        u_til = tf.matmul(a_weight, s2)
         return u_tile
 
     @classmethod
@@ -91,9 +95,11 @@ class BidafAttn(nn.Module):
         attended vectors of s1
         which words in s1 is most similar to each words in s2
         """
-        t1 = s1.size(1)
-        b_weight = F.softmax(torch.max(s, dim=2)[0], dim=-1).unsqueeze(1)  # [b, t2]
-        h_tile = torch.bmm(b_weight, s1).repeat(1, t1, 1)  # repeat to match s1 # [B, t1, D]
+        t1 = s1.shape[1]
+        b_weight = tf.reshape(tf.softmax(tf.max(s, 2)[0], -1),[-1,1])
+        h_tile = tf.tile(tf.matmul(b_weight, s1), [1, t1, 1])
+#         b_weight = F.softmax(torch.max(s, dim=2)[0], dim=-1).unsqueeze(1)  # [b, t2]
+#         h_tile = torch.bmm(b_weight, s1).repeat(1, t1, 1)  # repeat to match s1 # [B, t1, D]
         return h_tile
 
     def forward(self, s1, l1, s2, l2):
